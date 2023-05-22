4f81da55a064a4cdc545337f0e49fea5517960f7
Abdullah Al Redwan <61589799+Redwiat@users.noreply.github.com>
2023-05-22 23:13:58 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ translateBtn = document.querySelector("button"),
 
 selectTag.forEach((tag, id) => {
     for (let country_code in countries) {
-        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
+        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "bn-IN" ? "selected" : "";
         let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
         tag.insertAdjacentHTML("beforeend", option);
     }
